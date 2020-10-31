b2d54923a7d86a08db0ee5ffe890f2b24d3f7114
SumitomShome <sumitomshome@gmail.com>
2020-11-01 01:48:06 +0530

    [Sumitom] Add: Ability to add person to both Friends and Family

---
+++ AddressDatabaseFolder/UC11-OnePersonTwoTypes.sql
@@ -0,0 +1,3 @@
+INSERT INTO address_book VALUES
+('Aniket', 'Mukherjee', 'Salt Lake', 'Kolkata', 'West Bengal', 700064, 9073074916,'aneicmukherjee@gmail.com', 'Family'),
+('Aniket', 'Mukherjee', 'Salt Lake', 'Kolkata', 'West Bengal', 700064, 9073074916,'aneicmukherjee@gmail.com', 'Friends')
