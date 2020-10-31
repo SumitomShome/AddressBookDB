20273246f5c62742d6c38063b45fd52c12208138
SumitomShome <sumitomshome@gmail.com>
2020-11-01 00:24:18 +0530

    [Sumitom] Add: Ability to insert new contacts in address book

---
+++ AddressDatabaseFolder/UC3-InsertionOfNewContacts.sql
@@ -0,0 +1,3 @@
+INSERT INTO address_book VALUES
+('Sumitom', 'Shome', 'Ruby', 'Kolkata', 'West Bengal', 700107, 9433430056, 'sumitomshome@gmail.com'),
+('Rahul', 'Das', 'Jadavpur', 'Kolkata', 'West Bengal', 700054, 9903309542, 'rahuldas96@gmail.com')
