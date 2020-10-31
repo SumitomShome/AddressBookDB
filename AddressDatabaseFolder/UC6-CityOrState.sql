f448b7a869b447c5e2ff20b3ece631312954fde1
SumitomShome <sumitomshome@gmail.com>
2020-11-01 01:07:34 +0530

    [Sumitom] Add: Ability to retrieve person belonging to a city or state

---
+++ AddressDatabaseFolder/UC6-CityOrState.sql
@@ -0,0 +1,5 @@
+INSERT INTO address_book VALUES
+('Kaustav', 'Banerjee', 'Vashi', 'Mumbai', 'Maharashtra', 400703, 8910208143, 'kaustav.nik@gmail.com'),
+('Sayan', 'Sarkar', 'Epip', 'Bengaluru', 'Karnataka', 56066, 8902644616, 'sayansarkar@gmail.com')
+SELECT * FROM address_book WHERE city = 'Mumbai'
+SELECT * FROM address_book WHERE state = 'Karnataka'
